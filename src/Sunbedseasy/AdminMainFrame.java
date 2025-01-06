49e1816abed63a3d60cf6295375aa38845a1d9e6
Benguiher <conanmocha@yahoo.es>
2025-01-06 22:51:19 +0000

    corrección de código

--- src/Sunbedseasy/AdminMainFrame.java
+++ src/Sunbedseasy/AdminMainFrame.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AdminMainFrame extends JFrame {
-    //d 
+    
 	private static final long serialVersionUID = 1L;
 	public AdminMainFrame(int adminId) {
         setTitle("Admin Panel");
